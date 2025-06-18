dcf5b42e5d25153f66629c7819faeef4c948a697
davmik2601 <davmik2601@gmail.com>
2025-06-19 02:12:50 +0400

    missed @Public decorator for app root route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,9 +2,12 @@ import { Controller, Get, Res } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 
+import { Public } from '@Decorators/public.decorator';
+
 @ApiTags('Global')
 @Controller()
 export class AppController {
+  @Public()
   @Get('/')
   @ApiOperation({
     summary: 'Will Redirect to the API Documentation (Swagger) ("/docs")',
